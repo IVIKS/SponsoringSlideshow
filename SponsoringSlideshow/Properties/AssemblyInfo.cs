6ba909890e3fbd805569e31f6032a1cb968d9ce5
Manuel K. Steidel <manuel.steidel@live.de>
2020-06-25 22:31:58 +0200

    [VERSION] - Change version system to major minor bugfix releasecount.

--- SponsoringSlideshow/Properties/AssemblyInfo.cs
+++ SponsoringSlideshow/Properties/AssemblyInfo.cs
@@ -43,13 +43,13 @@ using System.Windows;
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 //
-//      Hauptversion
-//      Nebenversion
-//      Buildnummer
-//      Revision
+//      Major
+//      Minor
+//      Bugfix
+//      ReleaseCount
 //
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
